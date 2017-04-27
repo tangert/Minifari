a2e4b81c2ee075bde8cf7799f8b9e925aacb14d5
tangert <tyler@angert.com>
2017-04-27 12:40:13 -0400

    Initial Commit

---
+++ BarBrowse/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BarBrowse
+//
+//  Created by Tyler Angert on 4/27/17.
+//  Copyright © 2017 Tyler Angert. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
